ffa47481b99f23b12b86e3832d8fcc945ff58cf0
tbourgeat <tbourgeat@elcia.com>
2024-12-13 12:18:25 +0100

    WIP

--- FrontEnd/src/entities/Category.ts
+++ FrontEnd/src/entities/Category.ts
@@ -2,3 +2,23 @@ export interface ICategory{
     name: string
     color: string
 }
+
+export const Categories: ICategory[] = [
+    { name: "Abdominaux", color: "rgb(207, 100, 0)" },
+    { name: "Abducteurs", color: "rgb(207, 193, 0)" },
+    { name: "Adducteurs", color: "rgb(207, 193, 0)" },
+    { name: "Avant-bras", color: "rgb(0, 183, 255)" },
+    { name: "Bas du dos", color: "rgb(0, 183, 255)" },
+    { name: "Biceps", color: "rgb(0, 183, 255)" },
+    { name: "Cardio", color: "rgb(90, 0, 207)" },
+    { name: "Épaules", color: "rgb(41, 207, 0)" },
+    { name: "Fessier", color: "rgb(207, 193, 0)" },
+    { name: "Grand Dorsal", color: "rgb(0, 183, 255)" },
+    { name: "Haut du dos", color: "rgb(0, 183, 255)" },
+    { name: "Ischio-jambiers", color: "rgb(207, 193, 0)" },
+    { name: "Mollets", color: "rgb(207, 193, 0)" },
+    { name: "Quadriceps", color: "rgb(207, 193, 0)" },
+    { name: "Pectoraux", color: "rgb(41, 207, 0)" },
+    { name: "Trapèzes", color: "rgb(0, 183, 255)" },
+    { name: "Triceps", color: "rgb(41, 207, 0)" }
+]
