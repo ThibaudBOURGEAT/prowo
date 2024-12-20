d035002d7d1d374924bfc77388f1e60640f830c9
tbourgeat <tbourgeat@elcia.com>
2024-12-20 16:30:46 +0100

    WIP

--- FrontEnd/src/entities/Routine.ts
+++ FrontEnd/src/entities/Routine.ts
@@ -4,4 +4,15 @@ export interface IRoutine{
     id: string
     title: string
     categories: ICategory[]
+
+}
+
+interface ISet{
+    nbSeries: number
+    restTimeSecond: number
+    exercices: IExercice[]
+}
+
+interface IExercice{
+
 }
